afd588eb10ca54a6be2e51f7a20af15782d073ad
WylieYYYY <wylieyyyy@gmail.com>
2020-07-22 15:36:46 +0000

    Changed icons to images

--- scripts/lang.js
+++ scripts/lang.js
@@ -1,12 +1,13 @@
 "use strict";
 function lang_mouse_over(mousein) {
-	document.getElementById("lang").innerText = mousein ? locale[lang] : "🌐";
+	document.getElementById("lang_icon").style.display = mousein ? "none" : "inline";
+	document.getElementById("lang_name").innerText = mousein ? locale[lang] : "";
 }
 var lang_key_list = Object.keys(locale);
 var lang_rota = lang_key_list.indexOf(lang);
 function lang_change(callback) {
 	lang_rota = (lang_rota + 1) % lang_key_list.length;
 	lang = lang_key_list[lang_rota];
-	document.getElementById("lang").innerText = locale[lang];
+	document.getElementById("lang_name").innerText = locale[lang];
 	callback == undefined || callback();
 }
