912718750416fc0d8544cad18c4726ddca20e5a0
WylieYYYY <wylieyyyy@gmail.com>
2020-07-26 18:14:18 +0000

    Linted using Google standard

--- scripts/info.js
+++ scripts/info.js
@@ -1,46 +1,66 @@
-"use strict";
-function info_update(unit_index, info, ui_scale) {
-	info.style.opacity = 1;
-	info.className = "info bar";
-	info.innerHTML = "<b>--" + localise["info" + lang] + "--</b>";
-	unitinfo[unit_index].forEach(function(property, index) {
-		var skip = false;
-		Object.keys(locale).forEach(function(lang_opt) {
-			if (lang_opt == lang) return;
-			if (unitinfo[0][index].indexOf(lang_opt) != -1) skip = true;
-		});
-		if (property == null || skip) return;
-		var localised_key = unitinfo[0][index];
-		if (localised_key.indexOf(lang) == -1) localised_key += lang;
-		var localised_name = localise[localised_key];
-		if (localised_name == "") return;
-		if (localised_name == undefined) {
-			localised_name = unitinfo[0][index].replace(new RegExp("(" + Object.keys(locale).join("|") + ")", "g"), "");
-			localised_name = localised_name.replace(/([A-Z])/g, " $1")
-			localised_name = localised_name.replace(/^./, localised_name.charAt(0).toUpperCase()) + ": ";
-		}
-		info.innerHTML += "<br/><b>" + localised_name + "</b>" + property;
-	});
-	info.style.width = window.innerWidth / 2 * ui_scale + "px";
+/* exported infoSwitch */
+/* global info, lang, locale, localise, markerScale, selected:writable,
+       unitinfo */
+'use strict';
+
+/**
+ * Shows or updates info box according to language and specified unit.
+ * @param {number} unitIndex - Unit's index in unitinfo.
+ * @param {object} info - The info boxs' elements to be modified.
+ * @param {number} UIScale - Multiplier determining the scale of the box.
+ */
+function infoUpdate(unitIndex, info, UIScale) {
+  info.style.opacity = 1;
+  info.className = 'info bar';
+  info.innerHTML = '<b>--' + localise['info' + lang] + '--</b>';
+  unitinfo[unitIndex].forEach(function(property, index) {
+    var skip = false;
+    Object.keys(locale).forEach(function(langOpt) {
+      if (langOpt == lang) return;
+      if (unitinfo[0][index].indexOf(langOpt) != -1) skip = true;
+    });
+    if (property == null || skip) return;
+    var localisedKey = unitinfo[0][index];
+    if (localisedKey.indexOf(lang) == -1) localisedKey += lang;
+    var localisedName = localise[localisedKey];
+    if (localisedName == '') return;
+    if (localisedName == undefined) {
+      localisedName = unitinfo[0][index].replace(
+          new RegExp('(' + Object.keys(locale).join('|') + ')', 'g'), '');
+      localisedName = localisedName.replace(/([A-Z])/g, ' $1');
+      localisedName = localisedName.replace(/^./,
+          localisedName.charAt(0).toUpperCase()) + ': ';
+    }
+    info.innerHTML += '<br/><b>' + localisedName + '</b>' + property;
+  });
+  info.style.width = window.innerWidth / 2 * UIScale + 'px';
 }
 
-function display_status(unit_index) {
-	if ("ontouchstart" in document.documentElement) {
-		info.style.opacity = 0;
-	} else {
-		info.className = "status bar";
-		info.innerText = unitinfo[unit_index][unitinfo[0].indexOf("name" + lang)];
-		info.style.width = "auto";
-	}
+/**
+ * Displays the small unit name status bar if the device is not a touchscreen.
+ * @param {number} unitIndex - Unit's index in unitinfo.
+ */
+function displayStatus(unitIndex) {
+  if ('ontouchstart' in document.documentElement) {
+    info.style.opacity = 0;
+  } else {
+    info.className = 'status bar';
+    info.innerText = unitinfo[unitIndex][unitinfo[0].indexOf('name' + lang)];
+    info.style.width = 'auto';
+  }
 }
 
-function info_switch(unit_index) {
-	if (selected == 0) {
-		selected = unit_index;
-		info_update(unit_index, document.getElementById("info"), marker_scale);
-		info.className += " spaced-dock";
-	} else {
-		selected = 0;
-		display_status(unit_index);
-	}
+/**
+ * Displays info box if a marker is selected, displays status bar otherwise.
+ * @param {number} unitIndex - Unit's index in unitinfo.
+ */
+function infoSwitch(unitIndex) {
+  if (selected == 0) {
+    selected = unitIndex;
+    infoUpdate(unitIndex, document.getElementById('info'), markerScale);
+    info.className += ' spaced-dock';
+  } else {
+    selected = 0;
+    displayStatus(unitIndex);
+  }
 }
--- scripts/lang.js
+++ scripts/lang.js
@@ -1,13 +1,27 @@
-"use strict";
-function lang_mouse_over(mousein) {
-	document.getElementById("lang_icon").style.display = mousein ? "none" : "inline";
-	document.getElementById("lang_name").innerText = mousein ? locale[lang] : "";
+/* exported langChange, langMouseOver */
+/* global lang:writable, locale */
+'use strict';
+
+/**
+ * Changes button appearance between globe icon and language abbreviation.
+ * @param {boolean} mouseIn - Whether mouse is entering the button.
+ */
+function langMouseOver(mouseIn) {
+  document.getElementById('lang_icon').style.display = mouseIn ?
+      'none' : 'inline';
+  document.getElementById('lang_name').innerText = mouseIn ? locale[lang] : '';
 }
-var lang_key_list = Object.keys(locale);
-var lang_rota = lang_key_list.indexOf(lang);
-function lang_change(callback) {
-	lang_rota = (lang_rota + 1) % lang_key_list.length;
-	lang = lang_key_list[lang_rota];
-	document.getElementById("lang_name").innerText = locale[lang];
-	callback == undefined || callback();
+
+var langKeyList = Object.keys(locale);
+var langRota = langKeyList.indexOf(lang);
+
+/**
+ * Rotates the language and change button's language abbreviation.
+ * @param {function} callback - Called to change other elements' language.
+ */
+function langChange(callback) {
+  langRota = (langRota + 1) % langKeyList.length;
+  lang = langKeyList[langRota];
+  document.getElementById('lang_name').innerText = locale[lang];
+  callback == undefined || callback();
 }
